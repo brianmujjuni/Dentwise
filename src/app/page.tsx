7d1443efa4a60b0334617c86e6721dfe4692ab99
brianmujjuni <brianmujjuni@engineer.com>
2025-11-23 23:47:07 +0300

    create pricing elements

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import Header from "@/components/landing/Header";
 import Hero from "@/components/landing/Hero";
 import HowItWorks from "@/components/landing/HowItWorks";
 import PricingSection from "@/components/landing/PricingSection";
+import WhatToAsk from "@/components/landing/WhatToAsk";
 import { SignedOut, SignInButton, SignedIn } from "@clerk/nextjs";
 import React from "react";
 
@@ -13,6 +14,7 @@ function Home() {
       <Header />
       <Hero />
       <HowItWorks />
+      <WhatToAsk/> 
       <PricingSection />
       <CTA />
       <Footer />
